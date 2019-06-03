784046015369ce855a567e623101b93303edf791
Christian Rollfinke <christianrolli54@gmail.com>
2019-06-03 10:17:42 +0200

    added picture view functionality

--- src/main/java/fh/swei/PrimaryController.java
+++ src/main/java/fh/swei/PrimaryController.java
@@ -1,39 +1,77 @@
 package fh.swei;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-
-import com.drew.imaging.ImageMetadataReader;
-import com.drew.imaging.ImageProcessingException;
-import com.drew.metadata.Directory;
-import com.drew.metadata.Metadata;
-import com.drew.metadata.Tag;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class PrimaryController {
 
-    File imagePath = new File("E:/Dokumente/FH/SWEI_SEM4/photodb/pictures/1.jpg");
+    private static List<String> result;
+    private int picn;
+    public ImageView mainImg;
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
-        try {
-            imageViwer();
-        } catch (ImageProcessingException e) {
+    }
+
+    @FXML
+    public void initialize(){
+        getPics();
+    }
+
+
+    public  void getPics(){
+
+        try (Stream<Path> walk = Files.walk(Paths.get("pictures"))) {
+
+            result = walk.filter(Files::isRegularFile)
+                    .map(x -> x.toString()).collect(Collectors.toList());
+
+
+            Image img=new Image(new FileInputStream(result.get(picn)));
+            System.out.println(result.get(picn));
+            //result.forEach(System.out::println);
+            mainImg.setImage(img);
+
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-
-    private void imageViwer() throws ImageProcessingException, IOException {
-
-        Metadata metadata = ImageMetadataReader.readMetadata(imagePath);
-        for (Directory directory : metadata.getDirectories()) {
-            for (Tag tag : directory.getTags()) {
-                System.out.println(tag);
-            }
-        }
+    @FXML
+    private void nextPic() throws FileNotFoundException {
+        picn++;
+        picn=(picn %result.size());
+        Image img=new Image(new FileInputStream(result.get(picn)));
+        mainImg.setImage(img);
     }
+
+    @FXML
+    private void prevPic() throws FileNotFoundException {
+        picn--;
+        if(picn<0){
+            picn=picn+result.size();
+
+        }
+        picn=(picn %result.size());
+
+        Image img=new Image(new FileInputStream(result.get(picn)));
+        mainImg.setImage(img);
+
+    }
+
+
 
 
 }
